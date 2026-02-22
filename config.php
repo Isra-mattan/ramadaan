5a65d20c6347a4456d6ecee5bc5f723f32f1f958
Isra-mattan <misraali732@gmail.com>
2026-02-22 22:03:18 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
-$host = "localhost";
-$db = "ramadan_planner";
-$user = "root";
-$pass = "";
+$host = getenv('MYSQLHOST');
+$db   = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT');
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
     echo "Connection failed: " . $e->getMessage();
